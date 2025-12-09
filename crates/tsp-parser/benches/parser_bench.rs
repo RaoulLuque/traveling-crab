c9696185c9846d57fca7ec02b7cbf96fd950934e
Raoul Luqué <raoulsluque@gmail.com>
2025-12-09 20:57:49 +0100

    tests: Add further benchmarks

--- crates/tsp-parser/benches/parser_bench.rs
+++ crates/tsp-parser/benches/parser_bench.rs
@@ -1,11 +1,21 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use tsp_parser::parse_tsp_instance;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Parse \"first.tsp\"", |b| {
-        b.iter(|| parse_tsp_instance("../../instances/bench/first.tsp").unwrap())
+fn a280_parsing_benchmark(c: &mut Criterion) {
+    c.bench_function("Parse \"a280.tsp\"", |b| {
+        b.iter(|| parse_tsp_instance("../../instances/bench/a280.tsp").unwrap())
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+fn d18512_parsing_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("d18512_parsing");
+    group.sample_size(10);
+    group.bench_function("Parse \"d18512.tsp\"", |b| {
+        b.iter(|| parse_tsp_instance("../../instances/bench/d18512.tsp").unwrap())
+    });
+    group.finish();
+}
+
+criterion_group!(a280, a280_parsing_benchmark);
+criterion_group!(d18512, d18512_parsing_benchmark);
+criterion_main!(a280, d18512);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
 fn main() {
-    let current_path = std::env::current_dir().unwrap();
-    println!("Current directory: {:?}", current_path);
-    let tsp_instance = tsp_parser::parse_tsp_instance("instances/bench/first.tsp");
-    println!("{:?}", tsp_instance);
+    let tsp_instance = tsp_parser::parse_tsp_instance("instances/bench/big.tsp").unwrap();
 }
