723c73d210a40989573c2acc3c36ad44bdf3cc4f
Raoul Luqué <raoulsluque@gmail.com>
2025-12-30 16:45:06 +0100

    perf: Fix up benchmarks

--- crates/tsp-rust/src/main.rs
+++ crates/tsp-rust/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     env_logger::init();
 
     let tsp_instance =
-        tsp_parser::parse_tsp_instance("instances/tsplib_symmetric/eil76.tsp").unwrap();
+        tsp_parser::parse_tsp_instance("instances/tsplib_symmetric/berlin52.tsp").unwrap();
     // println!("Parsed TSP instance: {:?}", tsp_instance.raw_distances());
     let distances_non_symmetric = tsp_instance.distances().to_non_symmetric();
     let best_tour = held_karp(&distances_non_symmetric);
